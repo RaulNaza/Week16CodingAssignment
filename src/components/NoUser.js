6c8252493624981feb45943d29ba0035367dce07
Raul Nazario <rnazariogonzales@gmail.com>
2023-11-09 10:20:14 -0500

    updated the Error pages-minor typos

--- src/components/NoUser.js
+++ src/components/NoUser.js
@@ -1,18 +1,11 @@
-// import { useNavigate } from "react-router-dom";
 
 
 function NoUser () {
 
-    // const navigate = useNavigate();
-
-    // function handleClick () {
-    //     navigate('/', {replace: true})
-    // }
-
     return (
         <div className="mt-5">
-            <p className="fs-1 fw-bolder">404 Error... No User Found</p>
-            <p className="fs-4 fst-italic">No users has been found with those credentials. Please check review and try again or create a new user.</p>
+            <p className="fs-1 fw-bolder">Error... No User Found</p>
+            <p className="fs-4 fst-italic">No users has been found with those credentials. Please review and try again or create a new user.</p>
             <i className="bi bi-emoji-frown-fill fs-1" ></i>
         </div>
     )
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,10 +2,10 @@
 
 function NotFound () {
     return (
-        <>
-            <h1>Not Found. 404 Error</h1>
-            <p>This will be the component used for when a link does not match any of our paths.</p>
-        </>
+        <div className="mt-5">
+            <p className="fs-1 fw-bolder">404 Error... Page Not Found</p>
+            <i className="bi bi-emoji-frown-fill fs-1" ></i>
+        </div>
     )
 }
 
